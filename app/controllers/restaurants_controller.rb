b21cf2f62f0fbaa09eeed236be91fe6807eff5ed
Jose Matias <josematias92@gmail.com>
2020-06-10 19:42:47 -0500

    Almost

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,4 +11,23 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
 
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+
+    redirect_to restaurants_path
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+
+    redirect_to restaurant(@restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :review)
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,11 +9,8 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
-    if @review.save
-      redirect_to restaurant_path(@restaurant)
-    else
-      render :new
-    end
+    @review.save
+    redirect_to restaurant_path(@restaurant)
   end
 
   private
